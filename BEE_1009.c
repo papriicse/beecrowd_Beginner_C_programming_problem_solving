7e69af1564c6558e04046565f4569a10060751ae
papriicse <101578016+papriicse@users.noreply.github.com>
2022-03-19 19:00:05 +0600

    solve BEE_1009

---
+++ BEE_1009.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    double b,c,d;
+    char a;
+   // scanf("%c\n",&a);
+    scanf("%S %lf %lf",&a,&b,&c);
+    d=b+c*0.15;
+    printf("TOTAL = R$ %0.2lf\n",d);
+    return 0;
+}
