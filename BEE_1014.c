38192d7d0ced7d3398a7a3a298405f1b12bbe55d
papriicse <101578016+papriicse@users.noreply.github.com>
2022-03-24 00:21:51 +0600

    solve BEE_1014

--- BEE_1014.c
+++ BEE_1014.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int X;
+    float Y,consumption;
+    scanf("%d",&X);
+    scanf("%f",&Y);
+    consumption=X/Y;
+    printf("%0.3f km/l",consumption);
+    return 0;
 
+}
