f77fbbc8b28202d9e18b9e3889e57689f94970d8
papriicse <101578016+papriicse@users.noreply.github.com>
2022-03-27 20:55:11 +0600

    solve BEE_1020

--- BEE_1020.c
+++ BEE_1020.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int years,months,days,n,y;
+    scanf("%d",&n);
+    years=n/365;
+    y=n-(years*365);
+    months=y/30;
+    days=y-(months*30);
+    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n",years,months,days);
+    return 0;
 
+}
