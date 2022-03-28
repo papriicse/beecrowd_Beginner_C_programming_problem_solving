b9a7be587819e3f0520a9289a00d5f3bd3877077
papriicse <101578016+papriicse@users.noreply.github.com>
2022-03-28 19:21:20 +0600

    solve BEE_1035

---
+++ BEE_1035.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,C,D,X,Y;
+    scanf("%d%d%d%d",&A,&B,&C,&D);
+    X=A+B;
+    Y=C+D;
+    if(B>C&&D>A&&Y>X&&C>0&&D>0&&A%2==0)
+    {
+        printf("Valores aceitos\n");
+    }
+    else
+    {
+        printf("Valores nao aceitos\n");
+    }
+    return 0;
+
+}
