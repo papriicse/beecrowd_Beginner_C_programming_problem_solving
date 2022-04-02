e8a244cccd77d9b87be5091ee3b843ca766f2f51
papriicse <101578016+papriicse@users.noreply.github.com>
2022-04-02 22:00:17 +0600

    solve BEE_1041

---
+++ BEE_1040.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    float N1,N2,N3,N4,avg;
+    scanf("%0.1f %0.1f %0.1f %0.1f",&N1,&N2,&N3,&N4);
+    avg=((N1*2)+(N2*3)+(N3*4)+(N4*1))/(2+3+4+1);
+   // printf("media: %lf\n",avg);
+    if(avg>=7)
+    {
+        printf("Aluno aprovado");
+    }
+    else if(avg<5)
+    {
+        printf("Aluno reprovado");
+    }
+    else if(5<avg<6.9)
+    {
+        printf("Aluno em exame");
+    }
+
+printf("media: %lf\n",avg);
+
+
+
+
+
+
+
+}
